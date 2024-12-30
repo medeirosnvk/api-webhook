b67fe1017958598a8cc8f769c9daa8588f902e24
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2024-12-30 17:23:49 -0300

    check url

--- src/index.js
+++ src/index.js
@@ -11,52 +11,81 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(bodyParser.json());
 
+const checkWebhookAvailability = (callback) => {
+  const options = {
+    hostname: "cobrance.com.br",
+    path: "/santander2/webhook_boleto.php",
+    method: "HEAD", // Verifica apenas a disponibilidade
+  };
+
+  const request = https.request(options, (response) => {
+    callback(response.statusCode >= 200 && response.statusCode < 400);
+  });
+
+  request.on("error", (error) => {
+    console.error("Erro ao verificar o webhook:", error);
+    callback(false);
+  });
+
+  request.end();
+};
+
 app.post("/webhook", (req, res) => {
   const data = req.body;
 
   console.log(req.originalUrl);
   console.log(req.body);
 
-  try {
-    const postData = JSON.stringify(data);
+  checkWebhookAvailability((isAvailable) => {
+    if (!isAvailable) {
+      return res
+        .status(503)
+        .json({ error: "Webhook indisponível no momento." });
+    }
 
-    const options = {
-      hostname: "cobrance.com.br",
-      path: "/santander2/webhook_boleto.php",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Content-Length": postData.length,
-      },
-    };
+    try {
+      const postData = JSON.stringify(data);
 
-    const request = https.request(options, (response) => {
-      console.log(`statusCode: ${response.statusCode}`);
+      const options = {
+        hostname: "cobrance.com.br",
+        path: "/santander2/webhook_boleto.php",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Content-Length": postData.length,
+        },
+      };
 
-      response.on("data", (d) => {
-        process.stdout.write(d);
+      const request = https.request(options, (response) => {
+        console.log(`statusCode: ${response.statusCode}`);
+
+        response.on("data", (d) => {
+          process.stdout.write(d);
+        });
       });
-    });
 
-    request.on("error", (error) => {
-      console.error(error);
+      request.on("error", (error) => {
+        console.error(error);
+        res
+          .status(500)
+          .json({ error: "Erro ao enviar os dados para cobrance.com.br" });
+      });
+
+      request.write(postData);
+      request.end();
+
+      res.status(200).json();
+    } catch (error) {
+      console.error("Erro ao enviar os dados para cobrance.com.br:", error);
       res
         .status(500)
         .json({ error: "Erro ao enviar os dados para cobrance.com.br" });
-    });
-
-    request.write(postData);
-    request.end();
-
-    res.status(200).json();
-  } catch (error) {
-    console.error("Erro ao enviar os dados para cobrance.com.br:", error);
-    res
-      .status(500)
-      .json({ error: "Erro ao enviar os dados para cobrance.com.br" });
-  }
+    }
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Servidor HTTPS rodando em https://191.101.70.186:${port}`);
+  console.log(
+    `Servidor HTTPS rodando em https://santander.cobrance.online:${port}`
+  );
 });
