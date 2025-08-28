54cc41bd49ef9b540772be76c4ce3ebef6645dfe
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2025-08-28 10:55:40 -0300

    wip

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,13 +4,6 @@ module.exports = {
       name: "api-santander",
       script: "yarn",
       args: "start",
-      interpreter: "none",
-      env: {
-        NODE_ENV: "production",
-      },
-      pre_deploy: {
-        pre: "yarn install --production=false && yarn build",
-      },
     },
   ],
 };
